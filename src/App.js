307f76774662b35580cb5d75a080d4b9019d62af
Terry Liu <dassf2648@gmail.com>
2022-04-29 14:15:12 +0800

    fetch && get行政區

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import './App.scss';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.ris.gov.tw/rs-opendata/api/v1/datastore/ODRP019/106")
+      .then((r) => r.json())
+      .then((obj) => {
+        //確認抓到資料
+        // console.log(obj.responseData);
+
+        //行政區分類
+        obj.responseData.forEach(e => {
+          if (data.includes(e.site_id)) {
+            return
+          } else {
+            data.push(e.site_id)
+          }
+        });
+        console.log('data', data);
+        setData(data);
+      });
+  }, []);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="logoArea">
+          <div className="logo"></div>
+          <p>106年<br />人口戶數及性別</p>
+        </div>
+        <div className="select">
+          <select>
+            <option value="1">選擇行政區</option>
+            {data.map((v, i) => {
+              return <option value={v} key={i}>{v}</option>
+            })}
+          </select>
+        </div>
+      </div>
+    </>
   );
 }
 
