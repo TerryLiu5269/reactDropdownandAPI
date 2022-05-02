106163e84ccd37feb656841ea0c264fb9fe39e91
Terry Liu <dassf2648@gmail.com>
2022-05-02 16:42:54 +0800

    add chart.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,57 @@
 import React, { useState, useEffect } from "react";
 import './App.scss';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import faker from 'faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+//for <Bar/>
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
 
 
 function App() {
@@ -88,6 +140,7 @@ function App() {
           </select>
         </div>
         <div className="chartArea">
+          <Bar options={options} data={data} />;
         </div>
       </div>
     </>
@@ -95,7 +148,8 @@ function App() {
 }
 
 
-
 export default App;
+
+
 
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot, ReactDOM } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+
+const rootElement = document.getElementById('root');
+createRoot(rootElement).render(<App />);
 
