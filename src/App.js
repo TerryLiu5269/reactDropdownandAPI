90e5237f8e7ccf5e77de64fd5c9546c6945ff63b
Terry Liu <dassf2648@gmail.com>
2022-05-04 18:01:17 +0800

    rwd && down

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from "react";
 import './App.scss';
 import {
@@ -10,7 +11,6 @@ import {
   Legend,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
-import faker from 'faker';
 
 ChartJS.register(
   CategoryScale,
@@ -21,37 +21,7 @@ ChartJS.register(
   Legend
 );
 
-//for <Bar/>
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top',
-    },
-    title: {
-      display: true,
-      text: 'Chart.js Bar Chart',
-    },
-  },
-};
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    {
-      label: 'Dataset 2',
-      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-  ],
-};
 
 
 function App() {
@@ -64,6 +34,11 @@ function App() {
   const [HOFNum, setHOFNum] = useState(0);
   const [HSMNum, setHSMNum] = useState(0);
   const [HSFNum, setHSFNum] = useState(0);
+  // console.log(HOMNum, HOFNum, HSMNum, HSFNum)
+
+  //for <bar/>
+  const labels = ['共同生活戶', '獨立生活戶'];
+
 
   useEffect(() => {
     fetch("https://www.ris.gov.tw/rs-opendata/api/v1/datastore/ODRP019/106")
@@ -78,6 +53,7 @@ function App() {
         //行政區分類 (for option)
         const data = [];
         obj.responseData.forEach(e => {
+          //e.site_id.slice(3) > ex: '台北市大安區' 變成 '大安區'
           if (data.includes(e.site_id.slice(3))) {
             return
           } else {
@@ -100,47 +76,76 @@ function App() {
           <div className="logo"></div>
           <p>106年<br />人口戶數及性別</p>
         </div>
-        <div className="select">
-          <select onChange={(e) => {
-            // household_ordinary_m(共同生活戶_男)
-            // household_ordinary_f(共同生活戶_女)
-            // household_single_m(單獨生活戶_男)
-            // household_single_f(單獨生活戶_女)
+        <div className="showArea">
+          <div className="select">
+            <select onChange={(e) => {
+              // household_ordinary_m(共同生活戶_男)
+              // household_ordinary_f(共同生活戶_女)
+              // household_single_m(單獨生活戶_男)
+              // household_single_f(單獨生活戶_女)
 
-            let HOMArr = []
-            let HOFArr = []
-            let HSMArr = []
-            let HSFArr = []
+              let HOMArr = []
+              let HOFArr = []
+              let HSMArr = []
+              let HSFArr = []
 
-            src.forEach(el => {
-              if (e.target.value === '選擇行政區') {
-                setHOMNum(0);
-                setHOFNum(0);
-                setHSMNum(0);
-                setHSFNum(0);
-              }
-              if (el.site_id.includes(e.target.value)) {
-                HOMArr.push(parseInt(el.household_ordinary_m));
-                HOFArr.push(parseInt(el.household_ordinary_f));
-                HSMArr.push(parseInt(el.household_single_m));
-                HSFArr.push(parseInt(el.household_single_f));
-                setHOMNum(HOMArr.reduce((a, b) => a + b));
-                setHOFNum(HOFArr.reduce((a, b) => a + b));
-                setHSMNum(HSMArr.reduce((a, b) => a + b));
-                setHSFNum(HSFArr.reduce((a, b) => a + b));
-              } else {
-                return
-              }
-            });
-          }}>
-            <option value="選擇行政區">選擇行政區</option>
-            {data.map((v, i) => {
-              return <option value={v} key={i}>{v}</option>
-            })}
-          </select>
-        </div>
-        <div className="chartArea">
-          <Bar options={options} data={data} />;
+              src.forEach(el => {
+                if (e.target.value === '選擇行政區') {
+                  setHOMNum(0);
+                  setHOFNum(0);
+                  setHSMNum(0);
+                  setHSFNum(0);
+                }
+                if (el.site_id.includes(e.target.value)) {
+                  HOMArr.push(parseInt(el.household_ordinary_m));
+                  HOFArr.push(parseInt(el.household_ordinary_f));
+                  HSMArr.push(parseInt(el.household_single_m));
+                  HSFArr.push(parseInt(el.household_single_f));
+                  setHOMNum(HOMArr.reduce((a, b) => a + b));
+                  setHOFNum(HOFArr.reduce((a, b) => a + b));
+                  setHSMNum(HSMArr.reduce((a, b) => a + b));
+                  setHSFNum(HSFArr.reduce((a, b) => a + b));
+                } else {
+                  return
+                }
+              });
+            }}>
+              <option value="選擇行政區">選擇行政區</option>
+              {data.map((v, i) => {
+                return <option value={v} key={i}>{v}</option>
+              })}
+            </select>
+          </div>
+          <div className="chartArea">
+            <Bar className="barstyle"
+              options={{
+                responsive: true,
+                plugins: {
+                  legend: {
+                    position: 'top',
+                  },
+                  title: {
+                    display: false,
+                    text: 'Chart.js Bar Chart',
+                  }
+                }
+              }}
+              data={{
+                labels,
+                datasets: [
+                  {
+                    label: '女',
+                    data: [HOFNum, HSFNum],
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                  },
+                  {
+                    label: '男',
+                    data: [HOMNum, HSMNum],
+                    backgroundColor: 'rgba(53, 162, 235, 0.5)',
+                  },
+                ],
+              }} />
+          </div>
         </div>
       </div>
     </>
@@ -149,7 +154,3 @@ function App() {
 
 
 export default App;
-
-
-
-
--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import { createRoot, ReactDOM } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<App />);
 
 const rootElement = document.getElementById('root');
 createRoot(rootElement).render(<App />);
-
